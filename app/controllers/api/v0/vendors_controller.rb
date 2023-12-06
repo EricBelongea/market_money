41a482045337634ca89e3f6166f244f6a97e8b57
Eric Belongea <eric.belongea@gmail.com>
2023-12-06 11:06:31 -0700

    Create action

--- app/controllers/api/v0/vendors_controller.rb
+++ app/controllers/api/v0/vendors_controller.rb
@@ -3,9 +3,28 @@ class Api::V0::VendorsController < ApplicationController
 
   def show 
     render json: VendorSerializer.new(Vendor.find(params[:id]))
+    # require 'pry'; binding.pry
+  end
+
+  def create
+    # render json: VendorSerializer.new(vendor_params)
+    # require 'pry'; binding.pry
+    vendor = Vendor.new(vendor_params)
+    require 'pry'; binding.pry
+    if vendor.save
+      render json: VendorSerializer.new(vendor), message: "Vendor created successfully", status: :created
+      # render json: { message: "Vendor created successfully", vendor: VendorSerializer.new(vendor) }, status: :created
+      # require 'pry'; binding.pry
+    else
+      render json: { errors: your_resource.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
   private
+
+  def vendor_params
+    params.require(:vendor).permit(:name, :description, :contact_name, :contact_phone, :credit_accepted)
+  end
 
   def not_found_response(exception)
     render json: ErrorSerializer.new(ErrorMessage.new(exception.message, 404))
--- spec/requests/api/V0/vendors_request_spec.rb
+++ spec/requests/api/V0/vendors_request_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe "Vendor" do
     expect(response).to be_successful
 
     vendors = JSON.parse(response.body, symbolize_names: true)
+
     expect(vendors.count).to eq(1)
     vendor = vendors[:data]
 
@@ -31,6 +32,44 @@ RSpec.describe "Vendor" do
 
     expect(vendor[:attributes]).to have_key(:credit_accepted)
     expect(vendor[:attributes][:credit_accepted]).to be_a(TrueClass).or be_a(FalseClass)
+  end
+
+  it "Can send data to create a new Vendor" do
+    vendor_params = ({
+      name: "Turing",
+      description: "School of Software and Design",
+      contact_name: "Hello my name is yeff", 
+      contact_phone: "1-800-123-4567",
+      credit_accepted: true
+    })
+    headers = {"CONTENT_TYPE" => "application/json"}
+
+    post "/api/v0/vendors", headers: headers, params: JSON.generate(vendor: vendor_params)
+
+    created_vendor = JSON.parse(response.body, symbolize_names: true)
+    # created_vendor = Vendor.last
+    # require 'pry'; binding.pry
+    expect(response).to be_successful
+
+    expect(created_vendor[:data][:attributes]).to have_key(:name)
+    expect(created_vendor[:data][:attributes][:name]).to be_a(String)
+    expect(created_vendor[:data][:attributes][:name]).to eq(vendor_params[:name])
+
+    expect(created_vendor[:data][:attributes]).to have_key(:description)
+    expect(created_vendor[:data][:attributes][:description]).to be_a(String)
+    expect(created_vendor[:data][:attributes][:description]).to eq(vendor_params[:description])
+
+    expect(created_vendor[:data][:attributes]).to have_key(:contact_name)
+    expect(created_vendor[:data][:attributes][:contact_name]).to be_a(String)
+    expect(created_vendor[:data][:attributes][:contact_name]).to eq(vendor_params[:contact_name])
+
+    expect(created_vendor[:data][:attributes]).to have_key(:contact_phone)
+    expect(created_vendor[:data][:attributes][:contact_phone]).to be_a(String)
+    expect(created_vendor[:data][:attributes][:contact_phone]).to eq(vendor_params[:contact_phone])
+
+    expect(created_vendor[:data][:attributes]).to have_key(:credit_accepted)
+    expect(created_vendor[:data][:attributes][:credit_accepted]).to be_a(TrueClass).or(FalseClass)
+    expect(created_vendor[:data][:attributes][:credit_accepted]).to eq(vendor_params[:credit_accepted])
   end
 
   describe '#sad-path' do
